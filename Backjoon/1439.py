f165fe3e8f42cd20febc798534a5df04119f9731
정민지 <83000829+jungminji0215@users.noreply.github.com>
2021-12-12 00:03:42 +0900

    Update 1439.py

--- Backjoon/1439.py
+++ Backjoon/1439.py
@@ -3,13 +3,11 @@ S = list(input())
 count_0 = 0
 count_1 = 0
 
-
 if S[0] == "0":
     count_0 += 1
 else:
     count_1 += 1
-
-
+    
 for i in range(len(S)-1):
     if S[i] == "0":
         if S[i] != S[i+1]:
@@ -18,7 +16,6 @@ for i in range(len(S)-1):
     elif S[i] == "1":
         if S[i] != S[i+1]:
             count_0 += 1
-
 
 if(count_0 > count_1):
     print(count_1)
